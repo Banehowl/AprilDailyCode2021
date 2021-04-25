2d7083910062e1998198be9421f57f949deef6cf
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-25 08:41:15 -0700

    Roll a d20 (random.choice())
    Daily Code 4/25/2021

---
+++ DailyCode04252021.py
@@ -0,0 +1,13 @@
+# ---------------------------------------
+# #	Daily Code	04/25/2021
+#   "Roll a d20" Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------
+
+# Create code that rolls a d20
+
+import random
+
+dice_20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+print(random.choice(dice_20))
