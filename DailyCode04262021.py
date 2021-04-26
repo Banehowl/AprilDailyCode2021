cc2a33a392fb36f47bce699a2019d3959e53adda
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-26 08:08:17 -0700

    Roll a d4
    Daily Code for 04/26/2021

---
+++ DailyCode04262021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	04/26/2021
+#   "Roll a d4" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d4
+
+import random
+
+dice_4 = [1, 2, 3, 4]
+
+print(random.choice(dice_4))
