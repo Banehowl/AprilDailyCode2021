c15d966924214a260fcf449e5c96d700dda851ba
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-27 08:18:09 -0700

    Roll a d6
    Daily Code for 04/27/2021

---
+++ DailyCode04272021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	04/27/2021
+#   "Roll a d6" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d6
+
+import random
+
+dice_6 = [1, 2, 3, 4, 5, 6]
+
+print(random.choice(dice_6))
