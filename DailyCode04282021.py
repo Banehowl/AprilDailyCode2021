a199861e6b7054ac41a432eb13aad000f381c095
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-28 08:41:02 -0700

    Roll a d8
    Daily Code for 04/28/2021

---
+++ DailyCode04282021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	04/28/2021
+#   "Roll a d8" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d8
+
+import random
+
+dice_8 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(random.choice(dice_8))
