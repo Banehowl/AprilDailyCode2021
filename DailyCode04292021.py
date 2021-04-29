b8901b59a5cebee69f9af464c6e2d1299b6c7ea8
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-29 08:20:29 -0700

    Roll a d10
    Daily Code for 04/29/2021

---
+++ DailyCode04292021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	04/29/2021
+#   "Roll a d10" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d10
+
+import random
+
+dice_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(random.choice(dice_10))
