c13e3e5d79e78fe7c3c3539e5c715d42f8ff043c
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-04-30 06:30:53 -0700

    Roll a d12
    Daily Code for 04/30/2021

---
+++ DailyCode04302021.py
@@ -0,0 +1,13 @@
+# --------------------------------------
+# #	Daily Code	04/30/2021
+#   "Roll a d12" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------
+
+# Create code that rolls a d12
+
+import random
+
+dice_12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+print(random.choice(dice_12))
